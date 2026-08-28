de3da5b6d4f8122643c8cec2310b10811ee8af1d
TERENYANA <yanayusiv6447@gmail.com>
2026-08-28 15:52:33 +0200

    sub

---
+++ objs/expand/wildcard_dir.d
@@ -0,0 +1,18 @@
+objs/expand/wildcard_dir.o: srcs/expand/wildcard_dir.c \
+  includes/minishell.h \
+  /opt/homebrew/opt/readline/include/readline/readline.h \
+  /opt/homebrew/opt/readline/include/readline/rlstdc.h \
+  /opt/homebrew/opt/readline/include/readline/rltypedefs.h \
+  /opt/homebrew/opt/readline/include/readline/keymaps.h \
+  /opt/homebrew/opt/readline/include/readline/chardefs.h \
+  /opt/homebrew/opt/readline/include/readline/tilde.h \
+  /opt/homebrew/opt/readline/include/readline/history.h libft/libft.h
+includes/minishell.h:
+/opt/homebrew/opt/readline/include/readline/readline.h:
+/opt/homebrew/opt/readline/include/readline/rlstdc.h:
+/opt/homebrew/opt/readline/include/readline/rltypedefs.h:
+/opt/homebrew/opt/readline/include/readline/keymaps.h:
+/opt/homebrew/opt/readline/include/readline/chardefs.h:
+/opt/homebrew/opt/readline/include/readline/tilde.h:
+/opt/homebrew/opt/readline/include/readline/history.h:
+libft/libft.h:
---
+++ objs/parser/parser_nodes.d
@@ -0,0 +1,18 @@
+objs/parser/parser_nodes.o: srcs/parser/parser_nodes.c \
+  includes/minishell.h \
+  /opt/homebrew/opt/readline/include/readline/readline.h \
+  /opt/homebrew/opt/readline/include/readline/rlstdc.h \
+  /opt/homebrew/opt/readline/include/readline/rltypedefs.h \
+  /opt/homebrew/opt/readline/include/readline/keymaps.h \
+  /opt/homebrew/opt/readline/include/readline/chardefs.h \
+  /opt/homebrew/opt/readline/include/readline/tilde.h \
+  /opt/homebrew/opt/readline/include/readline/history.h libft/libft.h
+includes/minishell.h:
+/opt/homebrew/opt/readline/include/readline/readline.h:
+/opt/homebrew/opt/readline/include/readline/rlstdc.h:
+/opt/homebrew/opt/readline/include/readline/rltypedefs.h:
+/opt/homebrew/opt/readline/include/readline/keymaps.h:
+/opt/homebrew/opt/readline/include/readline/chardefs.h:
+/opt/homebrew/opt/readline/include/readline/tilde.h:
+/opt/homebrew/opt/readline/include/readline/history.h:
+libft/libft.h:
