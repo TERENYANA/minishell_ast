2607250f34aaef74eb68cc3f4824dcd83ea5470b
TERENYANA <yanayusiv6447@gmail.com>
2026-08-30 15:49:36 +0200

    done

--- srcs/exec/child.c
+++ srcs/exec/child.c
@@ -26,7 +26,11 @@ static void	exec_cmd_in_child(t_node *root, t_node *cur, t_exec_info *info)
 		cleanup_and_exit(root, info->env, 1);
 	close_heredoc_fds(root);
 	if (!cur->cmd || !cur->cmd[0])
+	{
+		close(STDIN_FILENO);
+		close(STDOUT_FILENO);
 		cleanup_and_exit(root, info->env, 0);
+	}
 	if (is_builtin(cur->cmd[0]) || is_env_builtin(cur->cmd))
 	{
 		if (ft_strcmp(cur->cmd[0], "exit") == 0)
