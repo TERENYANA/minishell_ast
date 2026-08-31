1c13045974a2e606ba067d3f1bf703e2f1c7f25d
TERENYANA <yanayusiv6447@gmail.com>
2026-08-31 18:29:55 +0200

    on_signal_heredoc

--- srcs/utils/signals_heredoc.c
+++ srcs/utils/signals_heredoc.c
@@ -14,18 +14,20 @@
 
 static void	on_signal_heredoc(int signo)
 {
-	(void)signo;
+	g_sig = signo;
 	write(STDOUT_FILENO, "\n", 1);
-	exit(130);
+	close(STDIN_FILENO);
 }
 
 void	setup_heredoc_signals(void)
 {
 	struct sigaction	sa;
 
+	g_sig = 0;
 	sa.sa_handler = on_signal_heredoc;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
 	sigaction(SIGINT, &sa, NULL);
 	signal(SIGQUIT, SIG_IGN);
+	rl_catch_signals = 0;
 }
